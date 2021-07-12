11e727b36df727e0bb59e4c28767dacf9cc4df51
tiagoguimaraesss <tiago.guimaraes@ulbra.inf.br>
2021-07-11 22:26:53 -0300

    :recycle: Refatoração na classe que monta o objeto de sessão para automação

--- src/main/java/br/com/tiago/config/AppiumSessionConfig.java
+++ src/main/java/br/com/tiago/config/AppiumSessionConfig.java
@@ -1,29 +1,26 @@
 package br.com.tiago.config;
 
-//import java.io.File;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-//import io.appium.java_client.remote.MobileCapabilityType;
-
 public class AppiumSessionConfig {
 
-    DesiredCapabilities capabilities = new DesiredCapabilities();
-
     public AndroidDriver<MobileElement> getAndroidDriverSession(String deviceName, String udid, int systemPort, String appiumServerUrl) throws MalformedURLException {
-    	capabilities.setCapability("deviceName", deviceName);
-    	capabilities.setCapability("udid", udid);
-    	capabilities.setCapability("platformName", "Android");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+    	capabilities.setCapability(MobileCapabilityType.UDID, udid);
         capabilities.setCapability("systemPort", systemPort);
-        capabilities.setCapability("skipDeviceInitialization", true);
-        capabilities.setCapability("skipServerInstallation", true);
-    	//capabilities.setCapability(MobileCapabilityType.APP, new File("app/app-debug.apk").getAbsolutePath());
-        capabilities.setCapability("appPackage", "br.com.lucasbieniek.aat");
-        capabilities.setCapability("appActivity", "br.com.lucasbieniek.aat.views.MainActivity");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+    	capabilities.setCapability(MobileCapabilityType.APP, new File("app/app-debug.apk").getAbsolutePath());
 
         return new AndroidDriver<>(new URL(appiumServerUrl), capabilities);
     }
